7c8e148ee181d1e1c8e46f757091c9af56cb8256
Magister0003 <126434642+Magister0003@users.noreply.github.com>
2023-06-21 15:07:00 -0500

    Add files via upload

--- menu_principal.php
+++ menu_principal.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
   <link rel="stylesheet" type="text/css" href="css/2.css">
-  <link rel="stylesheet" href="css/cards.css">
+  <link rel="stylesheet" href="css/2010.css">
   <!-- Favicon -->
   <link href="logo.png" rel="icon">
 
