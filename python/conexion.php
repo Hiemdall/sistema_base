d4a0f83d09d25874091952aa863d12ad39da7174
Heimdall Rojas <heimdallr20@gmail.com>
2023-07-02 11:24:09 -0500

    Desarrollo del proceso de guardar en python

---
+++ python/conexion.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "blockbl5_red_de_salud_oriente";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
